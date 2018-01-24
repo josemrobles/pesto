8fa16520e2094d317fe8b44e280036a3e0c83263
Jose Robles <jose.robles@1ksfl-mbp-jose.robles>
2018-01-23 21:06:44 -0500

    moar redis wip

--- api/batch.go
+++ api/batch.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"github.com/josemrobles/conejo"
 	"encoding/json"
+	"strconv"
 	"os"
 	"time"
 	"math/rand"
@@ -14,7 +15,7 @@ func getBatchID() string{
 	rand.Seed(time.Now().UnixNano())
 	var letterRunes = []rune("12345678abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-	b := make([]rune, 30)
+	b := make([]rune, 50)
     for i := range b {
         b[i] = letterRunes[rand.Intn(len(letterRunes))]
     }
@@ -30,7 +31,7 @@ func processBatch(b []byte) (string ,int,error) {
 	bID := getBatchID()
 
 	// Get total number of jobs in batch
-	numJobs := 5
+	numJobs := 500
 
 	if err != nil {
 
@@ -40,7 +41,6 @@ func processBatch(b []byte) (string ,int,error) {
 
 		// Add new batch to redis
 		redis.Do("SADD", "data:jobs", bID)
-		redis.Do("HSET", "stats:job:"+bID,"total_jobs",numJobs)
 
 		// Iterate through the payload and send each message
 		// @TODO - Actually iterate through the payload, currently a simulation
@@ -56,6 +56,11 @@ func processBatch(b []byte) (string ,int,error) {
 
 			} else {
 
+				// Convert item to string
+				item := strconv.Itoa(i+1)
+
+				// Set the status for the current job 0 = processing 1 = done 2 = error
+				redis.Do("HSET", "stats:job:"+bID,"job:"+item+":status",0)
 
 			} // Publish message
 
